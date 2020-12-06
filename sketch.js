01a04a7ebd7320bf1ba62935a0674c5800099d31
abayacoder <73934658+abayacoder@users.noreply.github.com>
2020-12-06 20:14:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
-var roof,Rope1,Rope2,Rope3,Rope4,Rope5,bobObject1, bobObject2, bobObject3, bobObject4, bobObject5
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+
+var roof1,Rope1,Rope2,Rope3,Rope4,Rope5,bobObject1, bobObject2, bobObject3, bobObject4, bobObject5
 
 function preload()
 {
@@ -10,35 +12,37 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	roof=Bodies.rectangle(350,100,300,30,)
-	World.add(world,roof)
+	//roof=Bodies.rectangle(350,100,300,30,)
+	//World.add(world,roof)
 	
 	
-	bobObject1= new bob(250,300);
-	bobObject2= new bob(300,300);
+	bobObject1= new bob(250,300,50);
+	bobObject2= new bob(300,300,50);
 	bobObject3= new bob(350,300);
-	bobObject4= new bob(400,300);
-	bobObjec5= new bob(450,300);
+	bobObject4= new bob(400,300,50);
+	bobObjec5= new bob(450,300,50);
 	
-  Rope1=new Rope (bobObject1.body,roof.body,-100,0);
+roof1=new roof (350,100,300,20);	
+	
+  Rope1=new Rope (bobObject1.body,roof1.body,-100,0);
   World.add(world,Rope1)	
   
-  Rope2=new Rope (bobObject1.body,roof.body,-50,0);
+  Rope2=new Rope (bobObject1.body,roof1.body,-50,0);
   World.add(world,Rope2)	
   
-  Rope3=new Rope (bobObject1.body,roof.body,-0,0);
+  Rope3=new Rope (bobObject1.body,roof1.body,-0,0);
   World.add(world,Rope3)	
   
-  Rope4=new Rope (bobObject1.body,roof.body,+50,0);
+  Rope4=new Rope (bobObject1.body,roof1.body,+50,0);
   World.add(world,Rope4)	
   
-  Rope5=new Rope (bobObject1.body,roof.body,+100,0);
+  Rope5=new Rope (bobObject1.body,roof1.body,+100,0);
   World.add(world,Rope5)	
   
  
@@ -47,10 +51,10 @@ function setup() {
 
 function draw() {
   
-  background("white");
+  background("lavender");
   Engine.udpate(engine)
 
-roof.display();
+roof1.display();
 
 
   bobObject1.display();
@@ -71,3 +75,12 @@ function keyPressed(){
 	if(KeyCode===32)
 	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:730,y:0})
 }
+
+
+
+
+
+
+
+
+
